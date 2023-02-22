ed4923e50133f4e91521c78e18cdc805c43a93b8
Kateryna Ostrynska <kateriinag@gmail.com>
2023-02-22 18:32:08 +0200

    add router.delete

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -48,9 +48,20 @@ router.post('/', async (req, res, next) => {
   }
 })
 
-// router.delete('/:contactId', async (req, res, next) => {
-//   res.json({ message: 'template message' })
-// })
+router.delete('/:id', async (req, res, next) => {
+  try {
+    const {id} = req.params;
+    const result = await contacts.removeContact(id);
+    if (!result) {
+      throw HttpError(404, 'Not found');
+    }
+    res.json({
+      message: "Contact deleted"
+    });  
+  } catch (error) {
+    next(error)
+  }
+})
 
 // router.put('/:contactId', async (req, res, next) => {
 //   res.json({ message: 'template message' })
