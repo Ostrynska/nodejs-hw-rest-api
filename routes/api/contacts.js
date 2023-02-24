dcf7422cc1fa1ae16a2ac98a4b0a701e49533a1d
Kateryna Ostrynska <kateriinag@gmail.com>
2023-02-24 16:45:22 +0200

    add patch for favorite

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -3,17 +3,20 @@ const express = require('express');
 const router = express.Router();
 
 const ctrl = require('../../controllers/contacts');
-const { validateBody, updateValidateBody } = require('../../middlewares');
-const schemas = require('../../schemas/contacts')
+const { validateBody, updateValidateBody, updateValidateFavorite, isValidId } = require('../../middlewares');
+
+const { schemas } = require('../../models/contact');
 
 router.get('/', ctrl.listContacts)
 
-router.get('/:id', ctrl.getContactById)
+router.get('/:id', isValidId, ctrl.getContactById)
 
 router.post('/', validateBody(schemas.addSchema), ctrl.addContact)
 
-router.delete('/:id', ctrl.removeContact)
+router.delete('/:id', isValidId, ctrl.removeContact)
 
-router.put('/:id',  updateValidateBody(schemas.addSchema), ctrl.updateContact)
+router.put('/:id', isValidId, updateValidateBody(schemas.addSchema), ctrl.updateContact)
+
+router.patch("/:id/favorite", isValidId, updateValidateFavorite(schemas.updateFavoriteSchema), ctrl.updateFavorite);
 
 module.exports = router;
