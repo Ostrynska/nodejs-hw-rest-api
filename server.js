65a2b0a7d2908504188b8a3cf8004c1b920575ff
Kateryna Ostrynska <kateriinag@gmail.com>
2023-02-24 14:10:31 +0200

    add npm dotenv and .env for  DB_HOST, PORT

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 const app = require('./app');
 
-const DB_HOST = 'mongodb+srv://Kateryna:Bbe9DAzEkdAEaFDM@cluster0.qqnzf5b.mongodb.net/db-contacts?retryWrites=true&w=majority';
+const { DB_HOST, PORT } = process.env;
 
 mongoose.set('strictQuery', true);
 
 mongoose.connect(DB_HOST)
   .then(() => {
-    app.listen(3000);
+    app.listen(PORT);
     console.log('Database connection successful');
   })
   .catch(error => {
