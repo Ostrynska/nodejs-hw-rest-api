85f1aa644eefa3d06fdd1bf8ac43f2ed135952e9
Kateryna Ostrynska <kateriinag@gmail.com>
2023-03-09 11:47:16 +0200

    add resize upload avatar by npm jimp

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const gravatar = require('gravatar');
+const Jimp = require('jimp');
 const path = require('path');
 const fs = require('fs/promises');
 
@@ -97,16 +98,19 @@ const updateSubscription = async (req, res) =>
 const updateAvatar = async (req, res) =>
 {
     const {_id} = req.user;
-    const { path: tempUpload, originalname } = req.file;
+    const { path: tmpUpload, originalname } = req.file;
     const filename = `${_id}_${originalname}`
+    const image = await Jimp.read(tmpUpload);
+    await image.resize(250, 250).writeAsync(tmpUpload);
     const resultUpload = path.join(avatarsDir, filename);
-    await fs.rename(tempUpload, resultUpload);
+    if (!tmpUpload) {
+        await fs.unlink(tmpUpload);
+        throw HttpError(401, "authorized");
+    }    
+    await fs.rename(tmpUpload, resultUpload);
     const avatarURL = path.join('avatars', filename);
     await User.findByIdAndUpdate(_id, { avatarURL });
-
-    res.json({
-        avatarURL,
-    })
+    res.json({ avatarURL });
 }
 
 module.exports = {
