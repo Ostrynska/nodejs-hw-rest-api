085a68af6da625ab9641e2a2ad8b854b21b25aed
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-03-16 16:53:46 +0200

    Update user.js

--- models/user.js
+++ models/user.js
@@ -36,10 +36,10 @@ const userSchema = new Schema({
         type: Boolean,
         default: false,
     },
-    verificationCode: {
+    verificationToken: {
         type: String,
-        default: ""
-    }
+        required: [true, 'Verify token is required'],
+    },
 }, {versionKey: false, timestamps: true});
 
 userSchema.post("save", handleMongooseError);
