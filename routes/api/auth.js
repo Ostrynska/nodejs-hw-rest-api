6d8564412315a577a9d0191fb11645fba81e5082
Kateryna Ostrynska <105549205+Ostrynska@users.noreply.github.com>
2023-07-12 15:53:09 +0300

    Update auth.js

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -16,7 +16,7 @@ router.get("/verify/:verificationToken", ctrl.verifyEmail);
 router.post("/verify", validateBody(schemas.emailSchema), ctrl.resendVerifyEmail);
 
 // signin
-router.get("/login", validateBody(schemas.loginSchema), ctrl.login);
+router.post("/login", validateBody(schemas.loginSchema), ctrl.login);
 
 router.get("/current", authenticate, ctrl.getCurrent);
 
